ae46297a4dd3f8306eeed2e4c002789591a30086
pdvega <pdvegaa@gmail.com>
2017-08-04 07:20:06 +0700

    showing item

---
+++ client/app.js
@@ -0,0 +1,28 @@
+new Vue({
+  el: '#app',
+  data: {
+    product: 'Point of Sales FA-5',
+    items: [
+        {
+          name: 'Aqua',
+          shortDesc: 500,
+          stock: 10
+        },
+        {
+          name: 'Coca Cola',
+          shortDesc: 3500,
+          stock: 5
+        },
+        {
+          name: 'Coffee',
+          shortDesc: 5000,
+          stock: 8
+        },
+        {
+          name: 'Pocari Sweat',
+          shortDesc: 7500,
+          stock: 7
+        }
+      ]
+  }
+})
